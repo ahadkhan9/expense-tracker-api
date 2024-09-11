d4686392a7c6b69dfc5455c07c99728d61d51947
Ahad Khan <ahadkhan5547@gmail.com>
2024-09-11 18:48:41 +0530

    Initial commit

---
+++ src/main/java/com/example/expensetracker/repositories/CategoryRepository.java
@@ -0,0 +1,21 @@
+package com.example.expensetracker.repositories;
+
+import com.example.expensetracker.domain.Category;
+import com.example.expensetracker.exceptions.EtBadRequestException;
+import com.example.expensetracker.exceptions.EtResourceNotFoundException;
+
+import java.util.List;
+
+public interface CategoryRepository {
+
+    List<Category> findAll(Integer userId) throws EtResourceNotFoundException;
+
+    Category findById(Integer userId, Integer categoryId) throws EtResourceNotFoundException;
+
+    Integer create(Integer userId, String title, String description) throws EtBadRequestException;
+
+    void update(Integer userId, Integer categoryId, Category category) throws EtBadRequestException;
+
+    void removeById(Integer userId, Integer categoryId);
+
+}
